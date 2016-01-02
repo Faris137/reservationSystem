a119d769b6aecbe61db7d3501993dd60bb5a9c71
Faris137 <farishassan2012@gmail.com>
2016-01-02 22:25:35 +0800

    Prototype
    Creating Forms and login.

---
+++ WindowsApplication1/Login.vb
@@ -0,0 +1,23 @@
+﻿Public Class Login
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim username, password As String
+        If TextBox1.Text = "student" And TextBox2.Text = "student123" Then
+            Student.Show()
+            Me.Hide()
+        ElseIf TextBox1.Text = "admin" And TextBox2.Text = "admin123" Then
+            Admin.Show()
+            Me.Hide()
+        Else
+            MsgBox("User name or Passwords is incorrect")
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+End Class
